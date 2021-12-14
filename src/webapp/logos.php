48404c6bdc1f7b63b2a1288446787522c8dcdd07
reimers <christian.reimers@univie.ac.at>
2021-12-14 17:29:43 +0100

    add global logo definition

---
+++ src/webapp/logos.php
@@ -0,0 +1,5 @@
+            <div class="sprint-logo">
+                <img src="img/sprint_1_64x64.png" alt="Logo SPRINT" width="45"><b><font style="font-family: Helvetica, sans-serif; font-size: 200%;">SPRINT</font></b>
+            </div>
+            <img src="img/uni_logo_220.jpg" alt="Logo University of Vienna" width="150" style="padding: 5px;"><br/>
+            <img src="img/csm_uni_logo_schwarz_0ca81bfdea.jpg" alt="Logo Institute for Astrophysics" width="150" style="padding: 5px;">

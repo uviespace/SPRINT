7a73e940bc8bd4ae4ec070a920087694ef70c87f
Armin Luntzer <armin.luntzer@univie.ac.at>
2025-01-27 11:41:47 +0100

    delete ancient crud

--- src/webapp/api/db_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define ("DB_USER", "user");
-define ("DB_PASSWORD", "pass");
-define ("DB_DATABASE", "testdb");
-define ("DB_HOST", "dev-mariadb-1");
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
-?>
